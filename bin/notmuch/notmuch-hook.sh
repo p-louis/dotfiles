b6501f533f3f8b3e5e2a8e15a17311007c82985f
Patrick Louis <patrick.louis@mailbox.org>
2017-09-25 09:16:41 +0200

    Added additional filters to notmuch-hook.sh

--- bin/notmuch/notmuch-hook.sh
+++ bin/notmuch/notmuch-hook.sh
@@ -2,29 +2,32 @@
 
 notmuch new
 
-newMail=$(notmuch search tag:unread | wc -l)
-if [[ $newMail -gt 0 ]]; then
-  sudo -u caya DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -i emblem-mail "New Mail" "You have $newMail new email"
-fi
-
 # serious stuff
 notmuch tag +bike-components -- from:info@bike-components.de
 notmuch tag +paypal -- from:@paypal.de or from:@paypal.com
 notmuch tag +amazon -- from:@amazon.de or from:@amazon.com
 notmuch tag +private +inbox -- from:louis and not tag:sent
 notmuch tag +steam -- from:@steampowered.com
-notmuch tag +msc -- from:@msccruises.de
+notmuch tag +msc -unread -inbox -- from:@msccruises.de
 notmuch tag +lotto -- from:info@staatliche-lotterieverwaltung.de
 notmuch tag +lieferheld -- from:@lieferheld.de
 notmuch tag +komoot -- from:komoot
-notmuch tag +espanien -- from:@espanien.de
-notmuch tag +ticket -- from:ticket@swm.de
-notmuch tag +soznet -unread -- from:@linkedin or from:@xing
+notmuch tag +espanien -unread -inbox -- from:@espanien.de
+notmuch tag +ticket -unread -- from:ticket@swm.de
+notmuch tag +soznet -unread -inbox -- from:@linkedin or from:@xing
+notmuch tag +studium -- from:@fernuni-hagen.de
 
 # Special Place in hell for those guys
-notmuch tag +newsletter -unread -inbox +killed -- from:marketing@weare.de or from:info@solidworks-community.com or from:news@ or from:newsletter@
-notmuch tag +facebook -unread -- from:@facebook
+notmuch tag +newsletter -unread -inbox +killed -- from:marketing@weare.de or from:info@solidworks-community.com or from:news@ or from:newsletter@ or from:vfe-campain-response@amazon.de
+notmuch tag +facebook -unread -inbox -- from:@facebook
 notmuch tag +youtube -unread -- from:@youtube.com
-notmuch tag -unread -- tag:killed
+notmuch tag +killed -unread -- from:@fmp.gmx.de or from:@gmxnet.de or from:@mail.verivox.de or from:@engage.vmware.com
+notmuch tag +duolingo -unread -inbox -- from:@duolingo.com
+notmuch tag -unread -inbox +killed -- from:promotion5@amazon.de or from:riotgames.com or from:@couchsurfing.com or from:@hackerrankmail.com
+notmuch tag -unread -inbox +killed -- from:@marketing.ti.com
+notmuch tag -unread -inbox -- tag:killed
 
-
+newMail=$(notmuch search tag:unread | wc -l)
+if [[ $newMail -gt 0 ]]; then
+  sudo -u caya DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -i emblem-mail "New Mail" "You have $newMail new email"
+fi
